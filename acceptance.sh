ad325b3ec49a3e0fbfdac4d727f950d3458b5045
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-09-15 01:13:20 +0300

    make big2 bigger

--- acceptance.sh
+++ acceptance.sh
@@ -17,7 +17,7 @@ if [ ! -d fixtures ]; then
     dd if=/dev/urandom of=medium bs=1048577 count=5
     dd if=/dev/urandom of=medium2 bs=1048577 count=5
     dd if=/dev/urandom of=big bs=1048577 count=10
-    dd if=/dev/urandom of=big2 bs=1048577 count=10
+    dd if=/dev/urandom of=big2 bs=1048577 count=50
     dd if=/dev/urandom of=mod512 bs=512 count=1
     dd if=/dev/urandom of=mod512double bs=512 count=2
     echo "Writing checks sums"
