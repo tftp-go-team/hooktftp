a62d2694d1d627c22431ad56e91e4f9b4f181d6b
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-09-21 22:16:47 +0300

    add regexp transform

---
+++ regexp_transform.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"errors"
+	// "fmt"
+	"regexp"
+	"strconv"
+)
+
+var NO_MATCH = errors.New("No match")
+var BAD_GROUPS = errors.New("Regexp has too few groups")
+type escape func(string) string
+type Transform func(string) (string, error)
+var fieldPat = regexp.MustCompile("\\$([0-9]+)")
+
+func NewRegexpTransform(regexpStr, template string, escape escape) (Transform, error) {
+	pat, err := regexp.Compile(regexpStr)
+	if err != nil {
+		return nil, err
+	}
+
+	return func(input string) (string, error) {
+		match := pat.FindAllStringSubmatch(input, -1)
+		if len(match) == 0 {
+			return "", NO_MATCH
+		}
+		fields := match[0]
+
+		var err error
+
+		output := fieldPat.ReplaceAllStringFunc(template, func (f string) string {
+			i, _ := strconv.Atoi(fieldPat.FindAllStringSubmatch(f, -1)[0][1])
+			if len(fields)-1 < i {
+				err = BAD_GROUPS
+				return ""
+			}
+			return escape(fields[i])
+		})
+
+		if err != nil {
+			return "", err
+		}
+
+		return output,  err
+
+	}, nil
+}
---
+++ regexp_transform_test.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"testing"
+)
+
+func dummyEscape(s string) string {
+	return s
+}
+
+type TransformTest struct {
+	regexp string
+	template string
+	escape escape
+	input string
+	output string
+	err error
+}
+
+var TransformTests = []TransformTest{
+	{
+		".*",
+		"$0",
+		dummyEscape,
+		"/var/tftpboot/hello",
+		"/var/tftpboot/hello",
+		nil,
+	},
+	{
+		"/var/tftpboot/(.*)$",
+		"http://localhost/get/$1",
+		dummyEscape,
+		"/var/tftpboot/hello",
+		"http://localhost/get/hello",
+		nil,
+	},
+	{
+		"/var/tftpboot/(.*)$",
+		"http://localhost/full$0",
+		dummyEscape,
+		"/var/tftpboot/hello",
+		"http://localhost/full/var/tftpboot/hello",
+		nil,
+	},
+	{
+		"/var/tftpboot/(.*)$",
+		"http://localhost/get$0",
+		dummyEscape,
+		"nomatch",
+		"",
+		NO_MATCH,
+	},
+	{
+		"/var/tftpboot/(.*)$",
+		"http://localhost/get/$1",
+		func(s string) string {
+			return "'" + s + "'"
+		},
+		"/var/tftpboot/hello",
+		"http://localhost/get/'hello'",
+		nil,
+	},
+	{
+		"/var/tftpboot/(.+)/(.+).txt$",
+		"http://localhost/get/$1/$2",
+		dummyEscape,
+		"/var/tftpboot/foo/bar.txt",
+		"http://localhost/get/foo/bar",
+		nil,
+	},
+	{
+		"/var/tftpboot/(.+)/(.+).txt$",
+		"http://localhost/get/$1",
+		dummyEscape,
+		"/var/tftpboot/foo/bar.txt",
+		"http://localhost/get/foo",
+		nil,
+	},
+	{
+		"/var/tftpboot/(.+)$",
+		"http://localhost/get/$1/$2",
+		dummyEscape,
+		"/var/tftpboot/foo/bar.txt",
+		"",
+		BAD_GROUPS,
+	},
+}
+
+func TestTranforms(t *testing.T) {
+	for _, tt := range TransformTests {
+
+		transform, err := NewRegexpTransform(
+			tt.regexp,
+			tt.template,
+			tt.escape,
+		)
+		if err != nil {
+			t.Error("failed to compile transform:", err, tt)
+			return
+		}
+
+		res, err := transform(tt.input)
+		if err != tt.err {
+			t.Errorf("Unexpected transform error '%v' expected '%v'\nfor '%v'", err, tt.err, tt)
+			return
+		}
+		if res != tt.output {
+			t.Errorf("Unexpected output '%v', expected '%v'\nfor '%v'", res, tt.output, tt)
+		}
+
+	}
+}
+
+
