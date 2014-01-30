3ac09985b035490ff38b5a5885ad26875165602b
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-01-30 11:35:38 +0000

    Document ci build process

--- ci.sh
+++ ci.sh
@@ -3,11 +3,13 @@
 set -eu
 set -x
 
-# https://go.googlecode.com/files/go1.2.linux-amd64.tar.gz
-# https://go.googlecode.com/files/go1.2.linux-386.tar.gz
-
 sudo apt-get update
 sudo apt-get install wget puavo-devscripts gcc --yes --force-yes
+
+# Too lazy to backport Go 1.2 for Ubuntu Precise. We'll just install the binary
+# manually.
+
+cd .. # get out of the source checkout
 
 arch=
 processor="$(gcc -dumpmachine)"
@@ -19,29 +21,32 @@ if [ "$arch" = "" ]; then
     exit 1
 fi
 
-cd ..
-
 go_bin_name="go1.2.linux-${arch}.tar.gz"
-
 wget -c "https://go.googlecode.com/files/${go_bin_name}"
 tar xzvf go*.tar.gz
 
 export GOROOT="$(pwd)/go"
 export PATH="$PATH:$GOROOT/bin"
 
+
+
+# Setup Go workspace for compiling
 mkdir workspace
 cd workspace
 
 export GOPATH="$(pwd)"
 
+# Move sources to the correct workspace location
 mkdir -p src/github.com/epeli
 mv ../hooktftp src/github.com/epeli
 cd src/github.com/epeli/hooktftp
 
+# Do Opinsys style debian package build
 puavo-build-debian-dir
 sudo puavo-install-deps debian/control
 puavo-dch $(cat VERSION)
 puavo-debuild
 
+# Upload packages to apt repositories
 aptirepo-upload -r $APTIREPO_REMOTE -b "git-$(echo "$GIT_BRANCH" | cut -d / -f 2)" ../hooktftp*.changes
 aptirepo-upload -r $APTIREPO_REMOTE -b hooktftp ../hooktftp*.changes
