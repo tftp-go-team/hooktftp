f4440938c1982698f22dff5303aca21df17e4677
Sten Spans <sspans@schubergphilis.com>
2015-07-06 08:50:12 +0200

    add tsize support

--- src/hooks/http_hook.go
+++ src/hooks/http_hook.go
@@ -7,8 +7,8 @@ import (
 )
 
 var HTTPHook = HookComponents{
-	func(url string) (io.ReadCloser, error) {
-		client := &http.Client {
+	func(url string) (io.ReadCloser, int, error) {
+		client := &http.Client{
 			CheckRedirect: func(req *http.Request, via []*http.Request) error {
 				if len(via) == 0 {
 					return nil
@@ -26,22 +26,22 @@ var HTTPHook = HookComponents{
 
 		req, err := http.NewRequest("GET", url, nil)
 		if err != nil {
-			return nil, err
+			return nil, -1, err
 		}
 
 		req.Header.Set("User-Agent", "hooktftp v0.9.1")
 
 		res, err := client.Do(req)
 		if err != nil {
-			return nil, err
+			return nil, -1, err
 		}
 
 		if res.StatusCode != http.StatusOK {
 			res.Body.Close()
-			return nil, fmt.Errorf("Bad response '%v' from %v", res.Status, url)
+			return nil, -1, fmt.Errorf("Bad response '%v' from %v", res.Status, url)
 		}
 
-		return res.Body, nil
+		return res.Body, int(res.ContentLength), nil
 
 	},
 	func(s string) string {
