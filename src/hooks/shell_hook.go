b72fe1ea17941503f342162df433c65b62f094be
Julien Castets <castets.j@gmail.com>
2015-10-26 11:47:19 +0100

    shell_hook: remove output buffering and log the exit status in finalizer

--- src/hooks/shell_hook.go
+++ src/hooks/shell_hook.go
@@ -1,16 +1,13 @@
 package hooks
 
 import (
-	"bytes"
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"os/exec"
 	"regexp"
 
 	"github.com/google/shlex"
-	"github.com/tftp-go-team/hooktftp/src/logger"
 	"github.com/tftp-go-team/libgotftp/src"
 )
 
@@ -48,41 +45,8 @@ var ShellHook = HookComponents{
 
 		err = cmd.Start()
 
-		// Buffering content to avoid Reader closing after cmd.Wait()
-		// For more informations please see:
-		//    http://stackoverflow.com/questions/20134095/why-do-i-get-bad-file-descriptor-in-this-go-program-using-stderr-and-ioutil-re
-		// Note:
-		//    This is not a perfect solution because of buffering. (Memory usage...)
-		//    If you have better solution ...
-		outOutput, err := ioutil.ReadAll(stdout)
-		if err != nil {
-			logger.Err("Shell output buffering failed: %v", err)
-		}
-
-		errOutput, err := ioutil.ReadAll(stderr)
-		if err != nil {
-			logger.Err("Shell stderr output buffering failed: %v", err)
-		}
-
-		// Use goroutine to log the exit status for debugging purposes.
-		// XXX: It probably is bad practice to access variables from multiple
-		// goroutines, but I hope it is ok in this case since the purpose is
-		// only to read and log the status. If not we must remove this bit.
-		// Please let me know if you know better.
-		go func() {
-			err := cmd.Wait()
-			if err != nil {
-				logger.Err("Command '%v' failed to execute: '%v'", command, err)
-			}
-		}()
-
-		return newHookResult(
-			ioutil.NopCloser(bytes.NewReader(outOutput)),
-			ioutil.NopCloser(bytes.NewReader(errOutput)),
-			-1,
-			nil,
-		), err
-
+		// cmd.Wait is used as Finalize to log the exit status.
+		return newHookResult(stdout, stderr, -1, cmd.Wait), err
 	},
 	func(s string) string {
 		return shellEscape.ReplaceAllStringFunc(s, func(s string) string {
